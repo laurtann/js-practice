d66b03ee92c431663bb80dd159539679e8e0e161
Laura Tannahill <lntannahill@gmail.com>
2021-02-08 09:00:18 -0400

    Ex of calc O by step

--- findNemo.js
+++ findNemo.js
@@ -18,4 +18,18 @@ function printFirstTwoBoxes(arr) {
   console.log(arr[2]);
 }
 
-printFirstTwoBoxes(boxes); // O(2) -> constant
+printFirstTwoBoxes(boxes); // O(2) -> constant; just use O(1). V scalable
+
+function funChallenge(input) {
+  let a = 10; // O(1)
+  a = 50 + 3; // O(1)
+
+  for (let i = 0; i < input.length; i++) { // O(n)
+    anotherFunction(); // O(n)
+    let stranger = true; // O(n)
+    a++; // O(n)
+  }
+  return a; // O(1)
+}
+
+funChallenge(); // O(4n + 3) -> debatable is assignments should be calc'd
