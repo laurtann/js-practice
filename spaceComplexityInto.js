f15628332fbecbe9dd68301c165f202a6169deb5
Laura Tannahill <lntannahill@gmail.com>
2021-02-08 09:54:59 -0400

    Ex space complexity

--- spaceComplexityInto.js
+++ spaceComplexityInto.js
@@ -7,11 +7,11 @@ function booo(n) {
 booo([1, 2, 3, 4, 5]); //O(1) - only care abt add'l inputs
 
 function arrayOfHiNTimes(n) {
-  let hiArr = [];
-  for (let i = 0; i < n; i++) { // let i = 0 var assignment
+  let hiArr = []; // new data struc => each item new memory space O(n)
+  for (let i = 0; i < n; i++) { //O(1)
     hiArr[i] = "hi";
   }
   console.log(hiArr);
 }
 
-arrayOfHiNTimes(6)
+arrayOfHiNTimes(6) // => O(n)
