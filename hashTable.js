d3e3ae76fbbfcafd0cdef91c82234b9c91ba854f
Laura Tannahill <lntannahill@gmail.com>
2021-02-09 16:48:29 -0400

    Add address to hashtable

--- hashTable.js
+++ hashTable.js
@@ -3,13 +3,6 @@ class HashTable {
     this.data = new Array(size);
   }
 
-  set(key, value) {
-    this.data[key] = value;
-  }
-
-  get(key) {
-    return this.data[key];
-  }
 
   _hash(key) {
     let hash = 0;
@@ -18,10 +11,33 @@ class HashTable {
     }
     return hash;
   }
+
+  set(key, value) {
+    let address = this._hash(key);
+    if (!this.data[address]) {
+      this.data[address] = [];
+    }
+    this.data[address].push([key, value]);
+    return this.data;
+  }
+
+  get(key) {
+    let address = this._hash(key);
+    const currBucket = this.data[address];
+    if (currBucket) {
+      for (let item of currBucket) {
+        if (item[0] === key) {
+          return item[1];
+        }
+      }
+    }
+    return undefined;
+  }
 }
 
+// memory space
 const myHashTable = new HashTable(50);
-myHashTable.set('grapes', 10000)
+console.log(myHashTable.set('grapes', 10000));
 console.log(myHashTable.get('grapes'));
 myHashTable.set('apples', 9)
 console.log(myHashTable.get('apples'));
